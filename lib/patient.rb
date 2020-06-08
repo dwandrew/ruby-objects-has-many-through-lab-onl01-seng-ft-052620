c24ab8205ea5d0fd08e8469a787702ff754d3252
Daniel Andrew <danwandrew@gmail.com>
2020-06-08 17:57:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,16 +19,6 @@ class Patient
         Appointment.new(date, self, doctor)
     end
     
-  #   describe '#new_appointment' do
-  #   it 'given a date and doctor, creates a new appointment belonging to that patient' do
-  #     doctor_who = Doctor.new('The Doctor')
-  #     hevydevy = Patient.new('Devin Townsend')
-  #     appointment = hevydevy.new_appointment('Friday, January 32nd', doctor_who)
-
-  #     expect(hevydevy.appointments).to include(appointment)
-  #     expect(appointment.patient).to eq(hevydevy)
-  #   end
-  # end
 
     def
 
