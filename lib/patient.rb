51da8caac76f337e801602691906db6b60f697d2
Daniel Andrew <danwandrew@gmail.com>
2020-06-08 17:39:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,17 +11,21 @@ class Patient
     save
     end
 
-    def new_appointment(date, doctor)
-        Appointment.new(date, self, doctor)
-    end
 
-    def appointments
-        Appointment.all.map{|k| k if k.patient.name == self.name}.select{|k| k}
-    end
+    def new_appointment
+    # def new_appointment(date, doctor)
+    #     Appointment.new(date, self, doctor)
+    # end
+
+    # def
+
+    # def appointments
+    #     Appointment.all.map{|k| k if k.patient.name == self.name}.select{|k| k}
+    # end
     
-    def doctors
-        Appointment.all.map{|k| k.doctor if k.patient.name == self.name}.select{|k| k}
-    end
+    # def doctors
+    #     Appointment.all.map{|k| k.doctor if k.patient.name == self.name}.select{|k| k}
+    # end
     
     def save
     @@all<<self
