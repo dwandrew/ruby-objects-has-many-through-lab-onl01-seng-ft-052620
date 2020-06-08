79bfef041c9b315e8134f9a5c2a2a5c23fbc0d56
Daniel Andrew <danwandrew@gmail.com>
2020-06-08 17:33:29 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,34 @@
+require_relative 'doctor.rb'
+require_relative 'appointment.rb'
+
+
+class Patient
+    attr_reader :name
+ @@all =[]   
+    
+    def initialize(name)
+    @name =name
+    save
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.map{|k| k if k.patient.name == self.name}.select{|k| k}
+    end
+    
+    def doctors
+        Appointment.all.map{|k| k.doctor if k.patient.name == self.name}.select{|k| k}
+    end
+    
+    def save
+    @@all<<self
+    end
+
+    def self.all
+    @@all
+    end
+
+end
